ec613256fe98458b0befb0a79ba39b69fe70b81b
ellin <ellin.xll@gmail.com>
2017-09-04 23:57:59 -0700

    Add segment tree.

--- cpp/source/SegmentTree.cpp
+++ cpp/source/SegmentTree.cpp
@@ -1,9 +1,36 @@
-//
-//  SegmentTree.cpp
-//  AlgSolutions
-//
-//  Created by LXU on 9/4/17.
-//  Copyright © 2017 LXU. All rights reserved.
-//
+#include "../Solutions.hpp"
+using namespace std;
 
-#include <stdio.h>
+/*************** Segment Tree **************/
+/*
+ We have an array arr[0 . . . n-1]. We should be able to
+ 1 Find the sum of elements from index l to r where 0 <= l <= r <= n-1
+ 
+ 2 Change value of a specified element of the array to a new value x. We need to do arr[i] = x where 0 <= i <= n-1.
+ */
+
+int fillST(vector<int>& st, int index, int start, int end, vector<int>& nums);
+
+//use array to store segment tree
+vector<int> constructST(vector<int> nums) {
+    int n = nums.size();
+    int leaves = 1;
+    while (leaves<n) {
+        leaves *= 2;
+    }
+    vector<int> res(2*leaves-1, 0);
+    fillST(res, 0, 0, n-1, nums);
+    return res;
+}
+
+int fillST(vector<int>& st, int index, int start, int end, vector<int>& nums) {
+    if (start==end) {
+        st[index] = nums[start];
+    } else {
+        int mid = start + (end-start)/2;
+        st[index] = fillST(st, 2*index+1, start, mid, nums) + fillST(st, 2*index+2, mid+1, end, nums);
+    }
+    return st[index];
+}
+
+
