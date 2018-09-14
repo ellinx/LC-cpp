5e9d13c8151e29252baf1011e5e5470187f1f38b
ellinx <ellin.xll@gmail.com>
2018-09-13 20:10:02 -0700

    Separate Single Number II and Single Number III.

---
+++ source/SingleNumberII.cpp
@@ -0,0 +1,28 @@
+#include "../Solutions.hpp"
+
+/*************** Single Number II **************/
+/*
+ Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+ Note:
+ Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+
+int Solutions::singleNumberII(vector<int>& nums) {
+    unordered_map<int,int> mp;
+    for (int num : nums) {
+        if (mp.find(num)==mp.end()) {
+            mp[num] = 1;
+        } else {
+            mp[num]++;
+        }
+    }
+
+    for (auto& kv : mp) {
+        if (kv.second==1) {
+            return kv.first;
+        }
+    }
+
+    return 0;
+}
--- source/SingleNumberIII.cpp
+++ source/SingleNumberIII.cpp
@@ -1,45 +1,18 @@
 #include "../Solutions.hpp"
 
-/*************** Single Number II **************/
-/*
- Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
- 
- Note:
- Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
- */
-
-int Solutions::singleNumberII(vector<int>& nums) {
-    unordered_map<int,int> mp;
-    for (int num : nums) {
-        if (mp.find(num)==mp.end()) {
-            mp[num] = 1;
-        } else {
-            mp[num]++;
-        }
-    }
-    
-    for (auto& kv : mp) {
-        if (kv.second==1) {
-            return kv.first;
-        }
-    }
-    
-    return 0;
-}
-
 /*************** Single Number III **************/
 /*
  Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
- 
+
  For example:
- 
+
  Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
  */
 
 vector<int> Solutions::singleNumberIII(vector<int>& nums) {
     unordered_map<int, int> mp;
     vector<int> res;
-    
+
     for (int num : nums) {
         if (mp.find(num)==mp.end()) {
             mp[num] = 1;
@@ -47,12 +20,12 @@ vector<int> Solutions::singleNumberIII(vector<int>& nums) {
             mp[num]++;
         }
     }
-    
+
     for (auto& kv : mp) {
         if (kv.second==1) {
             res.push_back(kv.first);
         }
     }
-    
+
     return res;
 }
