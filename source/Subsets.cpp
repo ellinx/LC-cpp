abb3c1736f9ee08b98a9ee0a3d83ad43dcecda24
ellinx <ellin.xll@gmail.com>
2018-09-13 20:20:07 -0700

    Separate Subsets and SubsetsII.

--- source/Subsets.cpp
+++ source/Subsets.cpp
@@ -3,12 +3,12 @@
 /***************** Subset *****************/
 /*
  Given a set of distinct integers, nums, return all possible subsets.
- 
+
  Note: The solution set must not contain duplicate subsets.
- 
+
  For example,
  If nums = [1,2,3], a solution is:
- 
+
  [
  [3],
  [1],
@@ -41,7 +41,7 @@ void helper(int start, int left, vector<int>& blk, vector<int>& nums, vector<vec
 
 vector<vector<int>> subsets(vector<int>& nums) {
     if (nums.empty()) return vector<vector<int>>();
-    
+
     vector<vector<int>> res;
     vector<int> blk;
     for (int i=0;i<=nums.size();i++) {
@@ -55,52 +55,10 @@ void helper(int start, int left, vector<int>& blk, vector<int>& nums, vector<vec
         res.push_back(blk);
         return;
     }
-    
+
     for (int i=start;i<=nums.size()-left;i++) {
         blk.push_back(nums[i]);
         helper(i+1,left-1,blk,nums,res);
         blk.pop_back();
     }
-}
-
-/***************** SubsetII *****************/
-
-/*
- Given a collection of integers that might contain duplicates, nums, return all possible subsets.
- 
- Note: The solution set must not contain duplicate subsets.
- 
- For example,
- If nums = [1,2,2], a solution is:
- 
- [
- [2],
- [1],
- [1,2,2],
- [2,2],
- [1,2],
- []
- ]
- */
-
-vector<vector<int>> Solutions::subsetsWithDup(vector<int>& nums) {
-    vector<vector<int>> res;
-    res.push_back(vector<int>());
-    sort(nums.begin(),nums.end());
-    
-    for (int i=0;i<nums.size();i++) {
-        int count = 1;
-        while(i<nums.size()-1 && nums[i]==nums[i+1]) {
-            count++;
-            i++;
-        }
-        vector<vector<int>> tmp = res;
-        for (int j=0;j<tmp.size();j++) {
-            for (int k=0;k<count;k++) {
-                tmp[j].push_back(nums[i]);
-                res.push_back(tmp[j]);
-            }
-        }
-    }
-    return res;
 }
---
+++ source/SubsetsII.cpp
@@ -0,0 +1,43 @@
+#include "../Solutions.hpp"
+
+/***************** SubsetII *****************/
+
+/*
+ Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+ Note: The solution set must not contain duplicate subsets.
+
+ For example,
+ If nums = [1,2,2], a solution is:
+
+ [
+ [2],
+ [1],
+ [1,2,2],
+ [2,2],
+ [1,2],
+ []
+ ]
+ */
+
+vector<vector<int>> Solutions::subsetsWithDup(vector<int>& nums) {
+    vector<vector<int>> res;
+    res.push_back(vector<int>());
+    sort(nums.begin(),nums.end());
+
+    for (int i=0;i<nums.size();i++) {
+        int count = 1;
+        while(i<nums.size()-1 && nums[i]==nums[i+1]) {
+            count++;
+            i++;
+        }
+        vector<vector<int>> tmp = res;
+        for (int j=0;j<tmp.size();j++) {
+            for (int k=0;k<count;k++) {
+                tmp[j].push_back(nums[i]);
+                res.push_back(tmp[j]);
+            }
+        }
+    }
+    return res;
+}
